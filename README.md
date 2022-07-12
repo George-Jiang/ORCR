73c634b92046bee388269d1bce50058f7c6b01b2
George Jiang 姜慧峰 <jianghuifeng@qq.com>
2022-07-12 16:11:34 +0800

    add space and corner rebar constraints
    add space and corner rebar constraints.
    - 增加约束条件
    - 钢筋间距约束
    - 角筋配筋量约束

--- README.md
+++ README.md
@@ -14,14 +14,46 @@
 
 相较于盈建科YJK等自动配筋的结果，利用本程序优化后，平均水平上，受力性能可提升10%，总钢筋用量可下降20%，即利用更少的材料实现更好的受力性能。
 
+#### 数学建模
+$$
+\min z = all\_As
+$$
+
+$$
+s.t.
+\left\{
+	\begin{align}
+		b\_A > b\_As \\
+		h\_A > h\_As \\
+		c\_A > c\_As \\
+		\sum b\_rebar\_co = 1 \\
+		\sum h\_rebar\_co = 1 \\
+		\sum c\_rebar\_co = 1 \\
+		50 \leq nt \leq 300 \\
+		pt \geq pt\_min \\
+		pt \geq bd,cd,hd \\
+		pt \leq 50
+	\end{align}
+\right.
+$$
+$nt$ 为net_space钢筋净间距
+$pt$ 为protection_thickness钢筋保护层厚度
 
 #### 使用方法
 ```python
 from orcr import optimize_column_rebar
-bn,bd,hn,hd,cd = optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_input=None)
+bn,bd,hn,hd,cd,pt,b_nt,h_nt = optimize_column_rebar(b,h,
+                                                    b_As_calc,h_As_calc,c_As_calc,
+                                                    stirrup_d=12,pt_min=20,
+                                                    bn_input=None,hn_input=None)
 #输入
+    #b,b边截面尺寸
+    #h,h边截面尺寸
     #b_As_calc,b边配筋计算值
     #h_As_calc,h边配筋计算值
+    #c_As_calc,角筋配筋计算值
+    #stirrup_d,箍筋直径
+    #pt_min,钢筋保护层最小厚度
     #bn_input,b边钢筋根数,默认为None,即程序自动判断
     #hn_input,h边钢筋根数,默认为None,即程序自动判断
 #输出
@@ -30,6 +62,9 @@ bn,bd,hn,hd,cd = optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_i
     #hn,h边钢筋根数
     #hd,h边钢筋直径
     #cd,角筋钢筋直径
+    #pt,protection_thickness,钢筋保护层厚度
+    #b_nt,b边钢筋净距
+    #h_nt,h边钢筋净距
 ```
 
 #### 备注
@@ -38,8 +73,12 @@ bn,bd,hn,hd,cd = optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_i
 - 若为整数，则此问题为MIP问题，即混合整数线性规划，求解时间约为0.02s
 
 ### to do list
-- 增加约束条件
-  - 钢筋间距约束
-  - 角筋配筋量约束
 - 增加批量处理功能
+- 增加CAD图纸生成功能
 
+
+### change log
+#### 2022-07-12
+- 增加约束条件
+- 钢筋间距约束
+- 角筋配筋量约束
--- orcr.py
+++ orcr.py
@@ -2,17 +2,23 @@ from pyscipopt import Model
 import numpy as np
 
 
-def optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_input=None):
+def optimize_column_rebar(
+    b,h,
+    b_As_calc,h_As_calc,c_As_calc,
+    stirrup_d=12,pt_min=20,
+    bn_input=None,hn_input=None):
     #输入
     #b_As_calc,b边配筋计算值
     #h_As_calc,h边配筋计算值
     model = Model()
 
-    rebar_list = np.array([12,14,16,18,20,22,25,28,30,32,35])
+    rebar_list = np.array([12,14,16,18,20,22,25,28,30,32,36,40])
     #可选钢筋的单根面积(cm2)
     rebar_area_list = np.pi * rebar_list **2 /4 /100
-
-    #b、h边钢筋数量
+    
+    ###添加变量
+    
+    #b、h边钢筋数量变量
     bn = model.addVar('bn', vtype='I')
     hn = model.addVar('hn', vtype='I')
     all_As = model.addVar('all_As')
@@ -21,6 +27,9 @@ def optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_input=None):
     b_rebar_co = np.array([model.addVar('b_rebar_d' + str(e), vtype = 'B') for e in rebar_list])
     h_rebar_co = np.array([model.addVar('h_rebar_d' + str(e), vtype = 'B') for e in rebar_list])
     c_rebar_co = np.array([model.addVar('c_rebar_d' + str(e), vtype = 'B') for e in rebar_list])
+    
+    #保护层厚度变量
+    pt = model.addVar('pt',vtype='C')
 
     #钢筋面积
     bA = b_rebar_co.dot(rebar_area_list)
@@ -35,8 +44,14 @@ def optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_input=None):
     #b、h边钢筋量
     b_As = 2*cA + bn * bA
     h_As = 2*cA + hn * hA
+    c_As = cA
+    
+    #钢筋净间距nt:net_space
+    b_nt = (b - 2*pt - 2*cd - 2*stirrup_d - bn*bd) / (bn + 1)
+    h_nt = (h - 2*pt - 2*cd - 2*stirrup_d - hn*hd) / (hn + 1)
 
-    #添加约束
+    
+    ###添加约束
 
     #添加单个类型选筋0-1变量约束
     model.addCons(sum(b_rebar_co) == 1)
@@ -46,19 +61,27 @@ def optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_input=None):
     #添加钢筋量约束
     model.addCons(b_As >= b_As_calc)
     model.addCons(h_As >= h_As_calc)
+    model.addCons(c_As >= c_As_calc)
 
-    #添加单边钢筋个数约束，以后需改成根据规范规则的约束
+    #添加单边钢筋个数约束，以钢筋净间距为约束50<= nt <=300
     if bn_input == None:
-        model.addCons(bn >= 1)
+        model.addCons(b_nt>=50)
+        model.addCons(b_nt<=300)
     else:
         model.addCons(bn == bn_input)
     if hn_input == None:
-        model.addCons(hn >= 1)
+        model.addCons(h_nt>=50)
+        model.addCons(h_nt<=300)
     else:
         model.addCons(hn == hn_input)
+        
+    #添加保护层厚度约束
+    model.addCons(pt >= pt_min)
+    model.addCons(pt >= bd)
+    model.addCons(pt >= hd)
+    model.addCons(pt >= cd)
+    model.addCons(pt <= 50)
     
-    #如果是预制柱需要的角部配筋，可以调整钢筋个数输入参数bn_input=2,hn_input=2
-
     #以约束方式添加目标函数，SCIP不允许目标函数为非线性，只能以约束的形式输入
     model.addCons(all_As == 4*cA + 2*bn*bA + 2*hn*hA)
 
@@ -74,6 +97,9 @@ def optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_input=None):
         bd_result = round(solve[bd])
         hd_result = round(solve[hd])
         cd_result = round(solve[cd])
+        pt_result = round(solve[pt])
+        b_nt_result = round((b - 2*pt_result - 2*cd_result - 2*stirrup_d - bn_result*bd_result)/(bn_result + 1))
+        h_nt_result = round((h - 2*pt_result - 2*cd_result - 2*stirrup_d - hn_result*hd_result)/(hn_result + 1))
         #print('结果:')
         #print('全部配筋梁(cm2):', obj)
         #print('b边钢筋量:', solve[b_As])
@@ -81,10 +107,10 @@ def optimize_column_rebar(b_As_calc, h_As_calc, bn_input=None, hn_input=None):
         #print('b:', str(bn_result) + 'D' + str(bd_result))
         #print('h:', str(hn_result) + 'D' + str(hd_result))
         #print('c:', '4D' + str(cd_result))
-        return(bn_result,bd_result,hn_result,hd_result,cd_result)
+        return([bn_result,bd_result,hn_result,hd_result,cd_result,pt_result,b_nt_result,h_nt_result])
 
     else:
-        #print('无法找到合适的解')
+        print('无法找到合适的解')
         return(None)
 
 
